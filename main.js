c928613da992c9c5dfefa263f98e02ae67329e4f
Jey-Cee <jey-cee@live.com>
2024-03-04 12:12:17 +0100

    reduce system load during discovery process

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ let pingTimeout = null;
 let taskList = [];
 let cronJob = null;
 
+
 const FORBIDDEN_CHARS = /[\]\[*,;'"`<>\\?]/g;
 
 class NetTools extends utils.Adapter {
@@ -322,54 +323,59 @@ class NetTools extends utils.Adapter {
 		try {
 			const ips = this.getIpRange();
 			const decimalSeparator = getDecimalSeparator();
- 			for(let ip in ips) {
-					ping.probe(ips[ip], {timeout: parseFloat(`0${decimalSeparator}25`), log: this.log.info}, async (error, result) => {
-						if (result.alive === true) {
-							result.mac = await arp.toMAC(result.host);
-							if (result.mac !== undefined && result.mac !== null) {
-								result.vendor = oui(result.mac)
-							} else {
-								return;
-							}
-							try {
-								result.name = await nslookup(result.host);
-							} catch (error) {
-								result.name = result.host;
-							}
-							let exists = false;
-
-							for (const entry of oldDevices) {
-								if (entry.native !== undefined && entry.native.mac === result.mac) {
-									exists = true;
-								}
-								if (exists === true && entry.native !== undefined && entry.native.ip !== result.host) {
-									const idName = result.mac.replace(/:/g, '');
-									await this.extendObjectAsync(this.namespace + '.' + idName, {
-										native: {
-											ip: result.host,
-											vendor: result.vendor
-										}
-									});
-								}
-							}
-							if (!exists) {
-								await this.addDevice(result.host, result.name, true, result.mac);
-							}
-						}
-					});
+			const promises = [];
+			for (let i = 0; i < ips.length; i += 10){
+				for(let j = 0; j < 10; j++) {
+					if (i + j < ips.length) {
+						promises.push(this.handleDiscoveryProbe(ips[i+j], oldDevices, decimalSeparator));
+					}
+				}
+				await Promise.all(promises);
 			}
-
-			const preparedObjects = await this.prepareObjectsByConfig();
-			 try {
-				 this.pingAll();
-			 } catch (error){
-				 this.log.error('Ping all: ' + error.toString());
-			 }
-			return true;
 		} catch (err) {
 			this.log.warn('Discovery faild: ' + err);
 			return false;
 		}
+	}
+
+	async handleDiscoveryProbe(ip, oldDevices, decimalSeparator){
+		return new Promise(async (resolve) => {
+			ping.probe(ip, {timeout: parseFloat(`0${decimalSeparator}25`), log: this.log.info}, async (error, result) => {
+				if (result.alive === true) {
+					result.mac = await arp.toMAC(result.host);
+					if (result.mac !== undefined && result.mac !== null) {
+						result.vendor = oui(result.mac)
+					} else {
+						return;
+					}
+					try {
+						result.name = await nslookup(result.host);
+					} catch (error) {
+						result.name = result.host;
+					}
+					let exists = false;
+
+					for (const entry of oldDevices) {
+						if (entry.native !== undefined && entry.native.mac === result.mac) {
+							exists = true;
+						}
+						if (exists === true && entry.native !== undefined && entry.native.ip !== result.host) {
+							const idName = result.mac.replace(/:/g, '');
+							await this.extendObjectAsync(this.namespace + '.' + idName, {
+								native: {
+									ip: result.host,
+									vendor: result.vendor
+								}
+							});
+						}
+					}
+					if (!exists) {
+						await this.addDevice(result.host, result.name, true, result.mac);
+					}
+				}
+				resolve(true);
+			});
+		})
 	}
 
 	/**
